41fa2095c55d2fc3f0e4ea1b44d68aaecaac5d90
Valentin Kalchev <valentin@zuant.com>
2020-06-05 10:21:14 +0300

    Create the FeedLoader boundary

---
+++ Feed/FeedLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedLoader.swift
+//  Feed
+//
+//  Created by Valentin Kalchev on 05/06/20.
+//  Copyright © 2020 Valentin Kalchev. All rights reserved.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
