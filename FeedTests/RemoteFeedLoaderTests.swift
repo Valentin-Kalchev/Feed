2f6ac9dda10a2ecaa1c2ad8c0b2ed7574b06af6c
Valentin Kalchev <valentin@zuant.com>
2020-06-16 11:02:01 +0300

    Move all mapping logic to the FeedItemsMapper

--- FeedTests/RemoteFeedLoaderTests.swift
+++ FeedTests/RemoteFeedLoaderTests.swift
@@ -102,8 +102,7 @@ class RemoteFeedLoaderTests: XCTestCase {
         return (sut, client)
     }
     
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
-        
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) { 
         addTeardownBlock { [weak instance] in
             XCTAssertNil(instance, "Instance should have been deallocated. Poterntial memory leak", file: file, line: line)
         }
