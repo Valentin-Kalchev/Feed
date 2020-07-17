44e458abaee2258a8ddefc04ff7bf6dbcb447024
Valentin Kalchev <valentin@zuant.com>
2020-07-17 15:38:40 +0300

    Save command succeeds on successful cache insertion

--- FeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ FeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -19,7 +19,7 @@ class LocalFeedLoader {
     }
     
     func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
-        store.deleteCachedFeed { [unowned self] error in 
+        store.deleteCachedFeed { [unowned self] error in
             if error == nil {
                 self.store.insert(items, timestamp: self.currentDate(), completion: completion)
             } else {
@@ -62,6 +62,10 @@ class FeedStore {
     
     func completeInsertion(with error: Error, at index: Int = 0) {
         insertionCompletions[index](error)
+    }
+    
+    func completeInsertionSuccessfully(at index: Int = 0) {
+        insertionCompletions[index](nil)
     }
 }
 
@@ -146,6 +150,26 @@ class CacheFeedUseCaseTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
         
         XCTAssertEqual(receivedError as NSError?, insertionError)
+    }
+    
+    func test_save_succeedsOnSuccessfulCacheInsertion() {
+        let items = [uniqueItem() , uniqueItem()]
+        let (sut, store) = makeSUT()
+            
+        let exp = expectation(description: "Wait for save completion")
+        var receivedError: Error?
+        
+        sut.save(items) { error in
+            receivedError = error
+            exp.fulfill()
+        }
+        
+        store.completeDeletionSuccessfully()
+        store.completeInsertionSuccessfully()
+        
+        wait(for: [exp], timeout: 1.0)
+        
+        XCTAssertNil(receivedError)
     }
     
     // MARK: - Helpers
