b76c447de8f173d8cf0262009bbe8e66ecb9b2da
Valentin Kalchev <valentin@zuant.com>
2020-07-22 14:50:56 +0300

    Move LocalFeedLoader and FeedStore collaborator to its own file in production

---
+++ Feed/Feed Cahce/FeedStore.swift
@@ -0,0 +1,17 @@
+//
+//  FeedStore.swift
+//  Feed
+//
+//  Created by Valentin Kalchev (Zuant) on 22/07/20.
+//  Copyright © 2020 Valentin Kalchev. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+}
