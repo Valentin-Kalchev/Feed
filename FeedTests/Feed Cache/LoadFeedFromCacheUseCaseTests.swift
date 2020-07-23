9868d9982eec6380e4cba992e7c2e3de6d7f79a2
Valentin Kalchev <valentin@zuant.com>
2020-07-23 11:46:25 +0300

    Load command delivers no images on 7 days old cache

--- FeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ FeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -52,6 +52,19 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         })
     }
     
+    func test_load_deliversNoImagesOnMoreThanSevenDaysOldCache() {
+        let feed = uniqueImageFeed()
+        
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimeStamp = fixedCurrentDate.adding(days: -7)
+        
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        expect(sut, toCompleteWith: .success([]), when: {
+            store.completeRetrieval(with: feed.local, timestamp: lessThanSevenDaysOldTimeStamp)
+        })
+    }
+    
     // MARK: - Helpers
      
     private func uniqueImageFeed() -> (models: [FeedImage], local: [LocalFeedImage]) {
