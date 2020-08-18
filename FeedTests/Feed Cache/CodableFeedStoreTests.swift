1994acd0d1462d47b200f8e7b41b2c46b3138e72
Valentin Kalchev <valentin@zuant.com>
2020-08-18 16:19:21 +0300

    Retrieving from empty cache delivers same empty result - no side effects

--- FeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ FeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -16,7 +16,6 @@ class CodableFeedStore {
 }
 
 class CodableFeedStoreTests: XCTestCase {
-
     func test_retrieve_deliversEmptyOnEmptyCache() {
         let sut = CodableFeedStore()
         
@@ -30,6 +29,26 @@ class CodableFeedStoreTests: XCTestCase {
             }
             
             exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1.0)
+    }
+ 
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        let sut = CodableFeedStore()
+        
+        let exp = expectation(description: "Wait for cache retrieval")
+        sut.retrieve { firstResult in
+            sut.retrieve { secondResult in
+                switch (firstResult, secondResult) {
+                case (.empty, .empty):
+                    break
+                default:
+                    XCTFail("Expected empty result, got \(firstResult) and \(secondResult) instead")
+                }
+                
+                exp.fulfill()
+            }
         }
         
         wait(for: [exp], timeout: 1.0)
