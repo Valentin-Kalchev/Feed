ea942541f1cddf0744a744c4fe5544285bbbcd25
Valentin Kalchev <valentin@zuant.com>
2020-09-15 11:49:34 +0300

    Delete delivers error on deletion error (URL with no delete permission)

--- FeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ FeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -76,8 +76,12 @@ class CodableFeedStore {
             return completion(nil)
         }
         
-        try! FileManager.default.removeItem(at: storeURL)
-        completion(nil)
+        do {
+            try FileManager.default.removeItem(at: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
     }
 }
 
@@ -174,6 +178,16 @@ class CodableFeedStoreTests: XCTestCase {
         XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
         expect(sut, toRetrieve: .empty)
     }
+    
+    func test_delete_deliversErrorOnDeletionError() {
+        let noDeletePermissionURL = cachesDirectory()
+        let sut = makeSUT(storeURL: noDeletePermissionURL)
+        
+        let deleteError = deleteCache(from: sut)
+        XCTAssertNotNil(deleteError, "Expect cache deletion to fail")
+        expect(sut, toRetrieve: .empty)
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(storeURL: URL? = nil, file: StaticString = #file, line: UInt = #line) -> CodableFeedStore {
@@ -196,6 +210,10 @@ class CodableFeedStoreTests: XCTestCase {
     
     private func deleteStoreArtefacts() {
         try? FileManager.default.removeItem(at: testSpecificStoreURL())
+    }
+    
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
     }
     
     private func expect(_ sut: CodableFeedStore, toRetrieveTwice expectedResult: RetrievedCachedFeedResult) {
