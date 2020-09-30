162a2aeb033875c7a86be29cc1cc7fcc341c8143
Valentin Kalchev <valentin@zuant.com>
2020-09-30 22:44:05 +0300

    Extract reusable CoreData helpers into a separate fiel

--- Feed/Infrastructure/CoreDataFeedStore.swift
+++ Feed/Infrastructure/CoreDataFeedStore.swift
@@ -65,40 +65,6 @@ public final class CoreDataFeedStore: FeedStore {
     }
 }
 
-private extension NSPersistentContainer {
-    
-    enum LoadingError: Swift.Error {
-        case modelNotFound
-        case failedToLoadPersistentStores(Swift.Error)
-    }
-    
-    static func load(with name: String, url: URL, in bundle: Bundle) throws ->  NSPersistentContainer {
-        guard let managedObjectModel = NSManagedObjectModel.load(with: name, in: bundle) else {
-            throw LoadingError.modelNotFound
-        }
-        
-        let description = NSPersistentStoreDescription(url: url)
-        let container = NSPersistentContainer(name: name, managedObjectModel: managedObjectModel)
-        container.persistentStoreDescriptions = [description]
-        
-        var loadError: Swift.Error?
-        container.loadPersistentStores { loadError = $1 }
-        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0)}
-        
-        return container
-    }
-}
-
-private extension NSManagedObjectModel {
-    static func load(with name: String, in bundle: Bundle) -> NSManagedObjectModel? {
-        guard let url = bundle.url(forResource: name, withExtension: "momd") else {
-            return nil
-        }
-        
-        return NSManagedObjectModel(contentsOf: url)
-    }
-}
-
 @objc(ManagedCache)
 class ManagedCache: NSManagedObject {
     @NSManaged var timestamp: Date
@@ -144,3 +110,4 @@ class ManagedFeedImage: NSManagedObject {
         return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
     }
 }
+
---
+++ Feed/Infrastructure/CoreDataHelpers.swift
@@ -0,0 +1,43 @@
+//
+//  CoreDataHelpers.swift
+//  Feed
+//
+//  Created by Valentin Kalchev (Zuant) on 30/09/20.
+//  Copyright © 2020 Valentin Kalchev. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+internal extension NSPersistentContainer {
+    enum LoadingError: Swift.Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Swift.Error)
+    }
+    
+    static func load(with name: String, url: URL, in bundle: Bundle) throws ->  NSPersistentContainer {
+        guard let managedObjectModel = NSManagedObjectModel.load(with: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+        
+        let description = NSPersistentStoreDescription(url: url)
+        let container = NSPersistentContainer(name: name, managedObjectModel: managedObjectModel)
+        container.persistentStoreDescriptions = [description]
+        
+        var loadError: Swift.Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0)}
+        
+        return container
+    }
+}
+
+private extension NSManagedObjectModel {
+    static func load(with name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        guard let url = bundle.url(forResource: name, withExtension: "momd") else {
+            return nil
+        }
+        
+        return NSManagedObjectModel(contentsOf: url)
+    }
+}
