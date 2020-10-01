692e03cdaa03417227973c4e3fee126b53698c6a
Valentin Kalchev <valentin@zuant.com>
2020-10-01 21:56:36 +0300

    Add "FeedCacheIntegrationTests" target to separate the potentially slower cache  integration tests from fast unit tests

---
+++ FeedCacheIntegrationTests/FeedCacheIntegrationTests.swift
@@ -0,0 +1,13 @@
+//
+//  FeedCacheIntegrationTests.swift
+//  FeedCacheIntegrationTests
+//
+//  Created by Valentin Kalchev (Zuant) on 01/10/20.
+//  Copyright © 2020 Valentin Kalchev. All rights reserved.
+//
+
+import XCTest
+
+class FeedCacheIntegrationTests: XCTestCase {
+    
+}
