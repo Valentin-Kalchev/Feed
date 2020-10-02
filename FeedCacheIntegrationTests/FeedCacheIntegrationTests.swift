97745eed04b935d0a88e469ee08ecf093c20d158
Valentin Kalchev <valentin@zuant.com>
2020-10-02 09:08:57 +0300

    Clean up & undo all cache side effects on 'setUp' and 'tearDown' to avoid sharing state between tests

--- FeedCacheIntegrationTests/FeedCacheIntegrationTests.swift
+++ FeedCacheIntegrationTests/FeedCacheIntegrationTests.swift
@@ -26,6 +26,42 @@ class FeedCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_load_deliversItemsSavedOnASeparateInstance() {
+        let sutToPerformSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let feed = uniqueImageFeed().models
+        
+        let saveExp = expectation(description: "Wait for save completion")
+        sutToPerformSave.save(feed) { (saveError) in
+            XCTAssertNil(saveError, "Expected to save feed successfully")
+            saveExp.fulfill()
+        }
+        wait(for: [saveExp], timeout: 1.0)
+        
+        let loadExp = expectation(description: "Wait for load completion")
+        sutToPerformLoad.load { (result) in
+            switch result {
+            case let .success(loadFeed):
+                XCTAssertEqual(feed, loadFeed)
+            case let .failure(error):
+                XCTFail("Expected successful feed result, got \(error) instead")
+            }
+            loadExp.fulfill()
+        }
+        
+        wait(for: [loadExp], timeout: 1.0)
+    }
+    
+    override func setUp() {
+        super.setUp()
+        setupEmptyStoreState()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        undoStoreSideEffects()
+    }
+    
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
         let storeBundle = Bundle(for: CoreDataFeedStore.self)
         let storeURL = testSpecificStoreURL()
@@ -42,5 +78,17 @@ class FeedCacheIntegrationTests: XCTestCase {
     
     private func cachesDirectory() -> URL {
         return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
+    
+    private func setupEmptyStoreState() {
+        deleteStoreArtefacts()
+    }
+    
+    private func undoStoreSideEffects() {
+        deleteStoreArtefacts()
+    }
+    
+    private func deleteStoreArtefacts() {
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
 }
