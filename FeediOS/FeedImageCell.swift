292fcd098e330e3e7f9ec180e302e5d269de5e3e
Valentin Kalchev <valentin@zuant.com>
2020-10-23 10:48:25 +0300

    Render loaded feed on successful load completion

---
+++ FeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  FeediOSTests
+//
+//  Created by Valentin Kalchev (Zuant) on 23/10/20.
+//  Copyright © 2020 Valentin Kalchev. All rights reserved.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
