e19f5d9ab8d1f90b6b953d22516843e8ddd3cad1
Valentin Kalchev <valentin@zuant.com>
2020-10-29 10:51:45 +0200

    Extract UIRefreshControl creation/configuration and refresh logic with 'FeedLoader' to the new 'FeedRefreshViewController'

---
+++ FeediOS/Feed Image Loader/FeedImageDataLoader.swift
@@ -0,0 +1,19 @@
+//
+//  FeedImageDataLoader.swift
+//  FeediOS
+//
+//  Created by Valentin Kalchev (Zuant) on 29/10/20.
+//  Copyright © 2020 Valentin Kalchev. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedImageDataLoaderTask {
+    func cancel()
+}
+
+public protocol FeedImageDataLoader {
+    typealias Result = Swift.Result<Data, Error>
+    
+    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+}
---
+++ FeediOS/Feed UI/Controllers/FeedRefreshViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FeedRefreshViewController.swift
+//  FeediOS
+//
+//  Created by Valentin Kalchev (Zuant) on 29/10/20.
+//  Copyright © 2020 Valentin Kalchev. All rights reserved.
+//
+
+import UIKit
+import Feed
+
+final class FeedRefreshViewController: NSObject {
+    private(set) lazy var view: UIRefreshControl = {
+        let view = UIRefreshControl()
+        view.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        return view
+    }()
+    
+    private let feedLoader: FeedLoader
+    init(feedLoader: FeedLoader) {
+        self.feedLoader = feedLoader
+    }
+    
+    var onRefresh: (([FeedImage]) -> Void)?
+    
+    @objc func refresh() {
+        view.beginRefreshing()
+        feedLoader.load { [weak self] result in
+            if let feed = try? result.get() {
+                self?.onRefresh?(feed)
+            }
+            self?.view.endRefreshing()
+        }
+    }
+}
