e47b2fa50a67f468fc3ac73191a01f5741d66579
Valentin Kalchev <valentin@zuant.com>
2021-01-12 14:34:43 +0000

    Move 'URLProtocolStub' to separate file

---
+++ FeedTests/Helpers/URLProtocolStub.swift
@@ -0,0 +1,66 @@
+//
+//  URLProtocolStub.swift
+//  FeedTests
+//
+//  Created by Valentin Kalchev (Zuant) on 12/01/21.
+//  Copyright © 2021 Valentin Kalchev. All rights reserved.
+//
+
+import Foundation
+
+class URLProtocolStub: URLProtocol {
+    private static var stub: Stub?
+    private static var requestObserver: ((URLRequest) -> Void)?
+    
+    private struct Stub {
+        let data: Data?
+        let response: URLResponse?
+        let error: Error?
+        let requestObserver: ((URLRequest) -> Void)?
+    }
+    
+    static func stub(data: Data?, response: URLResponse?, error: Error?) {
+        stub = Stub(data: data, response: response, error: error, requestObserver: nil)
+    }
+    
+    static func removeStub() {
+        stub = nil
+    }
+    
+    override class func canInit(with request: URLRequest) -> Bool {
+        return true
+    }
+    
+    override class func canonicalRequest(for request: URLRequest) -> URLRequest {
+        return request
+    }
+    
+    static func observeRequests(observer: @escaping (URLRequest) -> Void) {
+        stub = Stub(data: nil, response: nil, error: nil, requestObserver: observer)
+    }
+    
+    override func startLoading() {
+        guard let stub = URLProtocolStub.stub else {
+            return
+        }
+        
+        if let data = stub.data {
+            client?.urlProtocol(self, didLoad: data)
+        }
+        
+        if let response = stub.response {
+            client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
+        }
+        
+        if let error = stub.error {
+            client?.urlProtocol(self, didFailWithError: error)
+        } else {
+            client?.urlProtocolDidFinishLoading(self)
+        }
+        
+        stub.requestObserver?(request)
+        
+    }
+    
+    override func stopLoading() {}
+}
