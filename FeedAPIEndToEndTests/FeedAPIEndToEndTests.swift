adb81c4c97e837ac1a5cecda0f4ea2ba22ff949c
Valentin Kalchev <valentin@zuant.com>
2021-01-12 14:23:04 +0000

    Add 'RemoteFeedImageDataLoader' end-to-end test to guarantee both the client (app) and server (backend) respects the API contract, so we can fetch image data from remote.

--- FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
+++ FeedAPIEndToEndTests/FeedAPIEndToEndTests.swift
@@ -49,6 +49,17 @@ class FeedAPIEndToEndTests: XCTestCase {
         return receivedResult
     }
     
+    func test_endToEndTestServerGETFeedImageDataResult_matchesFixedTestAccountData() {
+        switch getFeedImageDataResult() {
+        case let .success(data)?:
+            XCTAssertFalse(data.isEmpty, "Expected non-empty image data")
+        case let .failure(error)?:
+            XCTFail()"Expected successful image data result, got \(error) instead")
+        default:
+            XCTFail("xpected successful image data result, got no result instead")s
+        }
+    }
+    
     // MARK: - Helpers
     
     private func expectedImage(at index: Int) -> FeedImage {
@@ -96,5 +107,24 @@ class FeedAPIEndToEndTests: XCTestCase {
 
     private func imageURL(at index: Int) -> URL {
         return URL(string: "https://url-\(index+1).com")!
+    }
+    
+    private func getFeedImageDataResult(file: StaticString = #file, line: UInt = #line) -> FeedImageDataLoader.Result? {
+        let testServerURL = URL(string: "https://essentialdeveloper.com/feed-case-study/test-api/feed/73A7F70C-75DA-4C2E-B5A3-EED40DC53AA6/image")!
+         let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+         let loader = RemoteFeedImageDataLoader(client: client)
+         trackForMemoryLeaks(client, file: file, line: line)
+         trackForMemoryLeaks(loader, file: file, line: line)
+
+         let exp = expectation(description: "Wait for load completion")
+
+         var receivedResult: FeedImageDataLoader.Result?
+         _ = loader.loadImageData(from: testServerURL) { result in
+             receivedResult = result
+             exp.fulfill()
+         }
+         wait(for: [exp], timeout: 5.0)
+
+         return receivedResult
     }
 }
