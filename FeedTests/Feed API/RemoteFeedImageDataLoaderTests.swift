76504f89e93f31fa471a2b30ed3b6bf13116febd
Valentin Kalchev <valentin@zuant.com>
2021-01-12 14:18:36 +0000

    Remove '@discardableResult' attribute - 'FeedimageDataLoaderTask' instance should be properly managed and never discarded.

--- FeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
+++ FeedTests/Feed API/RemoteFeedImageDataLoaderTests.swift
@@ -19,7 +19,7 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
     func test_loadingImageDataFromURL_requestDataFromURL() {
         let url = URL(string: "http://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
-        sut.loadImageData(from: url, completion: { _ in })
+        _ = sut.loadImageData(from: url, completion: { _ in })
         XCTAssertEqual(client.requestedURLs, [url])
     }
     
@@ -27,8 +27,8 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         let url = URL(string: "http://a-given-url.com")!
         let (sut, client) = makeSUT(url: url)
         
-        sut.loadImageData(from: url, completion: { _ in })
-        sut.loadImageData(from: url, completion: { _ in })
+        _ = sut.loadImageData(from: url, completion: { _ in })
+        _ = sut.loadImageData(from: url, completion: { _ in })
         
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
@@ -75,7 +75,7 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         var sut: RemoteFeedImageDataLoader? = RemoteFeedImageDataLoader(client: client)
         
         var capturedResults = [FeedImageDataLoader.Result]()
-        sut?.loadImageData(from: anyURL(), completion: { capturedResults.append($0)})
+        _ = sut?.loadImageData(from: anyURL(), completion: { capturedResults.append($0)})
         sut = nil
         client.complete(withStatusCode: 200, data: anyData())
         XCTAssertTrue(capturedResults.isEmpty)
@@ -123,7 +123,7 @@ class RemoteFeedImageDataLoaderTests: XCTestCase {
         let url = URL(string: "http://a-given-url.com")!
         let exp = expectation(description: "Wait for load completion")
         
-        sut.loadImageData(from: url) { (receivedResult) in
+        _ = sut.loadImageData(from: url) { (receivedResult) in
             switch (receivedResult, expectedResult) {
             case let (.success(receivedData), .success(expectedData)):
                 XCTAssertEqual(receivedData, expectedData, file: file, line: line)
