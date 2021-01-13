f924c149efe574c16d2527a8abee8249aaf1a26a
Valentin Kalchev <valentin@zuant.com>
2021-01-13 15:46:11 +0000

    Extract 'FeedImageDataStoreSpy' into a shared scope

---
+++ FeedTests/Feed Cache/FeedImageDataStoreSpy.swift
@@ -0,0 +1,37 @@
+//
+//  FeedImageDataStoreSpy.swift
+//  FeedTests
+//
+//  Created by Valentin Kalchev (Zuant) on 13/01/21.
+//  Copyright © 2021 Valentin Kalchev. All rights reserved.
+//
+
+import Foundation
+import Feed
+
+class FeedImageDataStoreSpy: FeedImageDataStore {
+    enum Message: Equatable {
+        case retrieve(dataFor: URL)
+        case insert(data: Data, for: URL)
+    }
+    
+    private(set) var receivedMessages = [Message]()
+    private var completions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+    
+    func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
+        receivedMessages.append(.insert(data: data, for: url))
+    }
+    
+    func retrieve(dataFromURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        receivedMessages.append(.retrieve(dataFor: url))
+        completions.append(completion)
+    }
+    
+    func completeRetrieval(with error: Error, at index: Int = 0) {
+        completions[index](.failure(error))
+    }
+    
+    func completeRetrieval(with data: Data?, at index: Int = 0) {
+        completions[index](.success(data))
+    }
+}
