66458454dd5dadb91c77c0524ff5304b1e645f7d
Valentin Kalchev <valentin@zuant.com>
2021-01-13 17:16:03 +0000

    Add test to guarantee that 'CoreDataFeedStore' side effects run serially to prevent unexpected behaviour

--- Feed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
+++ Feed/Feed Cache/Infrastructure/CoreData/CoreDataFeedStore+FeedImageDataLoader.swift
@@ -10,11 +10,12 @@ import CoreData
 
 extension CoreDataFeedStore: FeedImageDataStore {
     public func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
-        perform { (context) in
-            guard let image = try? ManagedFeedImage.first(with: url, in: context) else { return }
-            
-            image.data = data
-            try? context.save()
+        perform { (context) in  
+            completion(Result {
+                let image = try? ManagedFeedImage.first(with: url, in: context)
+                image?.data = data
+                try? context.save()
+            })
         }
     }
     
