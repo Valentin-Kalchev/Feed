41f3ba3ed51cfefe7ef281b2855811264dc8bc5e
Valentin Kalchev <valentin@zuant.com>
2021-02-01 14:16:25 +0000

    'FeedLoaderWithFeedbackComposite.load' delivers fallback feed on primary loader failure

--- EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
+++ EssentialApp/EssentialAppTests/FeedLoaderWithFallbackCompositeTests.swift
@@ -11,12 +11,22 @@ import Feed
 class FeedLoaderWithFallbackComposite: FeedLoader {
     
     private let primary: FeedLoader
+    private let fallback: FeedLoader
     init(primary: FeedLoader, fallback: FeedLoader) {
         self.primary = primary
+        self.fallback = fallback
     }
     
     func load(completion: @escaping (FeedLoader.Result) -> Void) {
-        primary.load(completion: completion)
+        primary.load { [weak self] (result) in 
+            switch result {
+            case .success:
+                completion(result)
+                
+            case .failure:
+                self?.fallback.load(completion: completion)
+            }
+        }
     }
 }
 
@@ -35,6 +45,27 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
                 
             case .failure:
                 XCTFail("Expected successful load feed result, got \(result) instead")
+            }
+            exp.fulfill()
+        }
+        
+        wait(for: [exp], timeout: 1)
+    }
+    
+    func test_load_deliversFallbackFreedOnPrimaryFailure() {
+        
+        let fallbackFeed = uniqueFeed()
+        let sut = makeSUT(primaryResult: .failure(anyNSError()), fallbackResult: .success(fallbackFeed))
+        
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { (result) in
+            switch result {
+            case let .success(receivedFeed):
+                XCTAssertEqual(receivedFeed, fallbackFeed)
+                
+            case .failure:
+                XCTFail("Expected successful load feed result, got \(result) instead")
+            
             }
             exp.fulfill()
         }
@@ -76,5 +107,9 @@ class FeedLoaderWithFallbackCompositeTests: XCTestCase {
         func load(completion: @escaping (FeedLoader.Result) -> Void) {
             completion(result)
         }
+    }
+    
+    private func anyNSError() -> NSError {
+        return NSError(domain: "any error", code: 0)
     }
 }
