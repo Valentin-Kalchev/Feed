7a0aae17083aa6552d4f336413c3c9024391f8db
Valentin Kalchev <valentin@zuant.com>
2021-02-24 11:31:36 +0000

    Record feed with content snapshot

--- EssentialFeed/FeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeed/FeediOSTests/FeedSnapshotTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+@testable import Feed
 import FeediOS
 
 class FeedSnapshotTests: XCTestCase {
@@ -14,6 +15,12 @@ class FeedSnapshotTests: XCTestCase {
         let sut = makeSUT()
         sut.display(emptyFeed())
         record(snapshot: sut.snapshot(), named: "EMPTY_FEED")
+    }
+    
+    func test_feedWithContent() {
+        let sut = makeSUT()
+        sut.display(feedWithContent())
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
     }
     
     //MARK: - Helpers
@@ -28,6 +35,19 @@ class FeedSnapshotTests: XCTestCase {
     
     private func emptyFeed() -> [FeedImageCellController] {
         return []
+    }
+    
+    private func feedWithContent() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: "The East Side Gallery is a n open-air gallery in Berlin. It consists of a serieds of murals painted directly on a 1,316 m long remnant of the Berlin Wall ...",
+                location: "Easy Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red)),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bengor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green))
+        ]
     }
     
     private func record(snapshot: UIImage, named name: String, file: StaticString = #file, line: UInt = #line) {
@@ -49,7 +69,6 @@ class FeedSnapshotTests: XCTestCase {
         } catch {
             XCTFail("Failed to record snapshot with error: \(error)", file: file, line: line)
         }
-        
     }
 }
 
@@ -59,5 +78,38 @@ extension UIViewController {
         return renderer.image { (action) in
             view.layer.render(in: action.cgContext)
         }
+    }
+}
+private extension FeedViewController {
+    func display(_ stubs: [ImageStub]) {
+        let cells: [FeedImageCellController] = stubs.map { (stub) -> FeedImageCellController in
+            let cellController = FeedImageCellController(delegate: stub)
+            stub.controller = cellController
+            return cellController
+        }
+        
+        display(cells)
+    }
+}
+
+private class ImageStub: FeedImageCellControllerDelegate {
+    let viewModel: FeedImageViewModel<UIImage>
+    weak var controller: FeedImageCellController?
+    
+    init(description: String?, location: String?, image: UIImage?) {
+        viewModel = FeedImageViewModel(
+            description: description,
+            location: location,
+            image: image,
+            isLoading: false,
+            shouldRetry: image == nil)
+    }
+    
+    func didRequestImage() {
+        controller?.display(viewModel)
+    }
+    
+    func didCancelImageRequest() {
+        
     }
 }
